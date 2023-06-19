86e7bf654f42daf421e48994ca6e4c72bd3a2fbc
prattakit <bave12342@gmail.com>
2023-06-19 11:08:34 +0700

    Dialog succeed

--- irc_application/lib/screen/export/export_screen.dart
+++ irc_application/lib/screen/export/export_screen.dart
@@ -21,8 +21,8 @@ class ExportScreen extends StatefulWidget {
 }
 
 class _ExportScreenState extends State<ExportScreen> {
-  List<CsvModel> csvModelData = [];
-  List<CsvModel> _tempCsv = [];
+  List<CsvScanModel> csvModelData = [];
+  List<CsvScanModel> _tempCsv = [];
   List<FileCsvScanModel> result = [];
   DatabaseHelper databaseHelper = DatabaseHelper();
   // final File file = File("/storage/emulated/0/Download/result.csv");
@@ -34,60 +34,85 @@ class _ExportScreenState extends State<ExportScreen> {
     _getData();
   }
 
+  Future _deleted() async {
+    if (result.isNotEmpty) {
+      await databaseHelper.deleted('FileScanCsv');
+
+      setState(() {
+        result.clear();
+        csvModelData.clear();
+      });
+      print("Test ${result.length}");
+    } else if (csvModelData.isNotEmpty) {
+      setState(() {
+        csvModelData.clear();
+      });
+    }
+  }
+
   Future<void> _ExportCSV() async {
-    final futures = <Future>[];
-    futures
-        .add(send("TIME", "DATE", "USER", "B1", "B2", "B3", "B4", "LOCATION"));
+    // final futures = <Future>[];
+    // futures
+    //     .add(send("TIME", "DATE", "USER", "B1", "B2", "B3", "B4", "LOCATION"));
 
     // df = pd.DataFrame({'name': ['John', 'Doe', 'Sam', 'Joel'],
     //   'age': [18, 10, 15, 32],
     //   'sex': ['Male', 'Female', 'Female', 'Male']
     // })
 
-    // var data = 'TIME';
-    // data += 'DATE';
-    // data += 'USER';
-    // data += 'B1';
-    // data += 'B2';
-    // data += 'B3';
-    // data += 'B4';
-    // data += 'LOCATION';
-    // data += '\r';
-    //
-    // var listData = result.length;
-    // if (result.isNotEmpty) {
-    //   result.forEach((element) {
-    //     data += element.TIME.toString();
-    //     data += element.DATE.toString();
-    //     data += element.USER.toString();
-    //     data += element.B1.toString();
-    //     data += element.B2.toString();
-    //     data += element.B3.toString();
-    //     data += element.B4.toString();
-    //     data += '\r';
-    //   });
-    // }
+    var data = 'TIME';
+    data += ',DATE';
+    data += ',USER';
+    data += ',B1';
+    data += ',B2';
+    data += ',B3';
+    data += ',B4';
+    data += ',LOCATION';
+    data += '\r';
 
-    // var selectDirectory = '/storage/emulated/0/Download';
-    // var filename = await getFilename('CSV');
-    // var pathFile = '$selectDirectory/$filename';
-    //
-    // var file = File(pathFile);
-    // var resultCSV = await file.writeAsString(data);
-  }
-
-  Future send(String TIME, String DATE, String USER, String B1, String B2,
-      String B3, String B4, String LOCATION) async {
-    try {
-      await file.writeAsString(
-          TIME += DATE += USER += B1 += B2 += B3 += B4 += LOCATION + '\n',
-          mode: FileMode.write,
-          flush: true);
-    } catch (e) {
-      print("Error: $e");
+    var listData = result.length;
+    if (result.isNotEmpty) {
+      result.forEach((element) {
+        data += '${element.TIME.toString()}';
+        data += ',${element.DATE.toString()}';
+        data += ',${element.USER.toString()}';
+        data += ',${element.B1.toString()}';
+        data += ',${element.B2.toString()}';
+        data += ',${element.B3.toString()}';
+        data += ',${element.B4.toString()}';
+        data += ',${element.LOCATION.toString()}';
+        data += '\r';
+      });
     }
-    return await file.length();
+
+    var selectDirectory = '/storage/101D-3E1F/Download/';
+    var filename = await getFilename('CSV');
+    var pathFile = '$selectDirectory/$filename';
+
+    var file = File(pathFile);
+    var resultCSV = await file.writeAsString(data);
+    _deleted();
+    _succeedDialog(
+        text: Label("Do you want to Delete ?"),
+        isHideCancle: false,
+        onpressOk: () async {
+          await _deleted();
+          Navigator.pop(context);
+        });
   }
+
+  // Future send(String TIME, String DATE, String USER, String B1, String B2,
+  //     String B3, String B4, String LOCATION) async {
+  //   try {
+  //     await file.writeAsString(
+  //         TIME += DATE += USER += B1 += B2 += B3 += B4 += LOCATION + '\n',
+  //         mode: FileMode.write,
+  //         flush: true);
+  //   } catch (e) {
+  //     print("Error: $e");
+  //   }
+  //   return await file.length();
+  // }
 
   Future _getData() async {
     List<Map<String, dynamic>> sqlData =
@@ -115,7 +140,7 @@ class _ExportScreenState extends State<ExportScreen> {
   // }
 
   Future<String> getFilename(String extension) async {
-    return '${DateTime.now().toIso8601String()}.extension';
+    return '${DateTime.now().toIso8601String().replaceAll('T', '').replaceAll('-', '').replaceAll(':', '').split('.')[0]}.$extension';
   }
 
   @override
@@ -135,8 +160,9 @@ class _ExportScreenState extends State<ExportScreen> {
             ),
             Center(
               child: CustomButton(
+                backgroundColor: COLOR_GREEN_LIGHT,
                 width: 300,
-                text: Label("Export Data"),
+                text: Label("Export Data", color: COLOR_WHITE),
                 onPressed: _ExportCSV,
               ),
             ),
@@ -155,7 +181,7 @@ class _ExportScreenState extends State<ExportScreen> {
               child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
                 child: Container(
-                  width: MediaQuery.of(context).size.width,
+                  // width: MediaQuery.of(context).size.width,
                   color: COLOR_WHITE,
                   child: DataTable(
                       border: TableBorder.all(width: 1),
@@ -204,6 +230,66 @@ class _ExportScreenState extends State<ExportScreen> {
             ))
           ],
         ),
+      ),
+    );
+  }
+
+  void _succeedDialog(
+      {Label? text,
+      Function? onpressOk,
+      Function? onpressCancel,
+      bool isHideCancle = true}) async {
+    // EasyLoading.showError("Error[03]", duration: Duration(seconds: 5));//if password
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        // title: const Text('AlertDialog Title'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Center(
+              child: text,
+            ),
+          ],
+        ),
+
+        actions: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Visibility(
+                visible: isHideCancle,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(COLOR_DANGER)),
+                  onPressed: () => Navigator.pop(context),
+                  child: const Label(
+                    'Cancel',
+                    color: COLOR_WHITE,
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: isHideCancle,
+                child: SizedBox(
+                  width: 15,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(COLOR_ACTIVE)),
+                  onPressed: () => onpressOk?.call(),
+                  child: const Label(
+                    'OK',
+                    color: COLOR_WHITE,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
