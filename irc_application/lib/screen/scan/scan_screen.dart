9cc85b2ba8de79dc924176d9fdeb6b4cd1a90146
aot <pattrawut_khamma@hotmail.com>
2023-06-19 14:53:30 +0700

    Edit SCAN SCREEN CHANGE  MESSAGE WARMNING PLEASE SCAN BARCODE

--- irc_application/lib/screen/scan/scan_screen.dart
+++ irc_application/lib/screen/scan/scan_screen.dart
@@ -223,7 +223,7 @@ class _ScanScreenState extends State<ScanScreen> {
                               : _locationController.text.isEmpty
                                   ? Label("Please Select Location")
                                   : _barcodeController.text.isEmpty
-                                      ? Label("Please Input User")
+                                      ? Label("Please Scan Barcode")
                                       : null,
                           onpressOk: () {
                             if (_userController.text.isEmpty) {
